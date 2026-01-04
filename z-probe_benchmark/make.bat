a5a9f8934dd334ece66d0990b3545d606c87e45d
Mathias Gruber <gruber.mathias@gmail.com>
2026-01-04 15:49:25 +0100

    Input shaper with lower belt tensions. Original SuperPinda benchmark

--- z-probe_benchmark/make.bat
+++ z-probe_benchmark/make.bat
@@ -1,1 +1,2 @@
 py -3 gen_stats.py --data=.\data\probe_accuracy_superpinda_fysetc.json --out=.\data\probe_accuracy_superpinda_fysetc.jpg --title="FYSETC Super Pinda" > Results.txt
+py -3 gen_stats.py --data=.\data\probe_accuracy_superpinda.json --out=.\data\probe_accuracy_superpinda.jpg --title="Prusa Super Pinda" > Results.txt
